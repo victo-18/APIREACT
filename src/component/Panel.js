58626b011120ada45e021be5793124faa03b426b
alomia18 <alomia18@gmail.com>
2023-04-05 01:17:42 -0500

    generando estilos para los personajes

--- src/component/Panel.js
+++ src/component/Panel.js
@@ -3,12 +3,27 @@ import "bootstrap/dist/css/bootstrap.min.css";
 export function Panel(props) {
   const { personaje } = props;
   return (
-    <div className="card">
-      <div className="contenedor__avatar">
-        <img src={personaje.image} className="avatar" alt="..." />
-      </div>
+    // <div className="card">
+    //   <div className="contenedor__avatar">
+    //
+    //   </div>
 
-      <div className="body">
+    //   <div className="body">
+    //     <h1>{personaje.name}</h1>
+    //     <ul>
+    //       <li>{personaje.status}</li>
+    //       <li>{personaje.gender}</li>
+    //       <li>{personaje.species}</li>
+    //     </ul>
+    //   </div>
+    // </div>
+    <div className="border">
+      <div className="contenedor">
+        <div className="contenedor__avatar">
+          <img src={personaje.image} className="avatar" alt="..." />
+        </div>
+      </div>
+      <div className="contenedor__info">
         <h1>{personaje.name}</h1>
         <ul>
           <li>{personaje.status}</li>
