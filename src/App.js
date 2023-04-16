5e7f6728304b4e062dd5896f9f5ade55a4b3755c
Javier-C0L <javier.castrillon@correounivalle.edu.co>
2023-04-15 19:47:09 -0500

    funcional

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
-//import logo from './logo.svg';
-import { React, useEffect ,useState} from "react";
-import "./App.css";
-import { Panel } from "./component/Panel";
-import { useFetch } from "./component/useFetch";
-import axios from "axios";
+import Main from './components/Main.jsx';
+
 function App() {
-  const [data, setData] = useState(null);
-  useEffect(() => {
-    const apiFetch = async () => {
-      const { status, data } = await axios.get(
-        "https://rickandmortyapi.com/api/character"
-      );
-      if (status === 200) {
-        setData(data.results);
-      }
-    };
-    if(data==null)
-    apiFetch();
-  });
-  return <>{data && data.map((pj) => <Panel personaje={pj} />)}</>;
+  return (
+    <>
+    <Main/>
+    </>
+  );
 }
 
 export default App;
